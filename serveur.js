f1e0424db792ff28497eb81f00b3154652aec05b
qwertyx <marci.yannis@gmail.com>
2017-08-24 15:06:17 +0200

    formulaire effectuer de connexion

--- serveur.js
+++ serveur.js
@@ -3,8 +3,8 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.listen(8080, function(err){
-    if(err){
+app.listen(8080, function(err) {
+    if (err) {
         console.error(err);
         return;
     }
