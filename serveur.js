50510cd7ae4a0629f118e6d2c5aa33a6974cbd8c
Apdil <apdil.aydinalp@gmail.com>
2017-08-24 14:48:26 +0200

    create direcory public

---
+++ serveur.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(8080, function(err){
+    if(err){
+        console.error(err);
+        return;
+    }
+    console.log('Connected');
+})
