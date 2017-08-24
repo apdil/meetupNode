9208b822b3c79d9a3aeae66f2132c350ab435a96
Apdil <apdil.aydinalp@gmail.com>
2017-08-24 15:12:24 +0200

    correction sreveur.js

--- serveur.js
+++ serveur.js
@@ -3,10 +3,6 @@ const app = express();
 
 app.use(express.static('public'));
 
-<<<<<<< HEAD
-app.listen(8080, function(err) {
-    if (err) {
-=======
 app.post(
     "/",
     bodyParser.urlencoded({ extended: true }),
@@ -18,7 +14,6 @@ app.post(
 
 app.listen(8080, function(err){
     if(err){
->>>>>>> d5e73a342c38bc350443d90e006ca92b918e96a7
         console.error(err);
         return;
     }
