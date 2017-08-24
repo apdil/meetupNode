d5e73a342c38bc350443d90e006ca92b918e96a7
Apdil <apdil.aydinalp@gmail.com>
2017-08-24 15:04:35 +0200

    add post in serveur

--- serveur.js
+++ serveur.js
@@ -3,6 +3,15 @@ const app = express();
 
 app.use(express.static('public'));
 
+app.post(
+    "/",
+    bodyParser.urlencoded({ extended: true }),
+    function(request, response) {
+        console.log(request.body);
+        response.send("Success!")
+    }
+);
+
 app.listen(8080, function(err){
     if(err){
         console.error(err);
